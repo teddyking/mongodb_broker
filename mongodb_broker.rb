eacf57ecf64e553c0f943df30be7595bfd7cc0a8
teddyking <ed@teddyking.co.uk>
2014-03-03 10:01:34 +0000

    added (very) basic auth

--- mongodb_broker.rb
+++ mongodb_broker.rb
@@ -6,7 +6,23 @@ class MongodbBroker < Sinatra::Base
     content_type 'application/json'
   end
 
+  use Rack::Auth::Basic do |username, password|
+    credentials = self.app_settings[:basic_auth]
+    username == credentials[:username] and password == credentials[:password]
+  end
+
   get '/v2/catalog' do
 
+  end
+
+  private
+
+  def self.app_settings
+    {
+      :basic_auth => {
+        :username => 'admin',
+        :password => 'admin'
+      }
+    }
   end
 end
--- spec/mongodb_broker_spec.rb
+++ spec/mongodb_broker_spec.rb
@@ -2,9 +2,19 @@ require_relative './spec_helper'
 
 describe MongodbBroker do
   describe 'GET /v2/catalog' do
-    it 'returns an HTTP 200' do
-      get '/v2/catalog'
-      last_response.status.should eq 200
+    context "when Basic Auth is not provided" do
+      it 'returns an HTTP 401' do
+        get '/v2/catalog'
+        last_response.status.should eq 401
+      end
+    end
+
+    context "when valid Basic Auth is provided" do
+      it 'returns an HTTP 200' do
+        authorize 'admin', 'admin'
+        get '/v2/catalog'
+        last_response.status.should eq 200
+      end
     end
   end
 end
