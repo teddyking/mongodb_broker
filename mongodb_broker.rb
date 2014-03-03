cd45cdc87ff7e634b6bba5f9dd75ed48b48ef517
teddyking <ed@teddyking.co.uk>
2014-03-03 10:05:56 +0000

    additional auth test + rubocoped

--- mongodb_broker.rb
+++ mongodb_broker.rb
@@ -7,7 +7,7 @@ class MongodbBroker < Sinatra::Base
   end
 
   use Rack::Auth::Basic do |username, password|
-    credentials = self.app_settings[:basic_auth]
+    credentials = app_settings[:basic_auth]
     username == credentials[:username] and password == credentials[:password]
   end
 
@@ -19,10 +19,7 @@ class MongodbBroker < Sinatra::Base
 
   def self.app_settings
     {
-      :basic_auth => {
-        :username => 'admin',
-        :password => 'admin'
-      }
+      basic_auth: { username: 'admin', password: 'admin' }
     }
   end
 end
--- spec/mongodb_broker_spec.rb
+++ spec/mongodb_broker_spec.rb
@@ -2,14 +2,22 @@ require_relative './spec_helper'
 
 describe MongodbBroker do
   describe 'GET /v2/catalog' do
-    context "when Basic Auth is not provided" do
+    context 'when Basic Auth is not provided' do
       it 'returns an HTTP 401' do
         get '/v2/catalog'
         last_response.status.should eq 401
       end
     end
 
-    context "when valid Basic Auth is provided" do
+    context 'when invalid Basic Auth is provided' do
+      it 'returns an HTTP 401' do
+        authorize 'badname', 'badpass'
+        get '/v2/catalog'
+        last_response.status.should eq 401
+      end
+    end
+
+    context 'when valid Basic Auth is provided' do
       it 'returns an HTTP 200' do
         authorize 'admin', 'admin'
         get '/v2/catalog'
