8658f60d9c5df3ed5df0351a84a8e06738fd50db
teddyking <ed@teddyking.co.uk>
2014-03-24 07:30:46 +0000

    removed need to prefix yml keys with :

--- mongodb_broker.rb
+++ mongodb_broker.rb
@@ -85,7 +85,24 @@ class MongodbBroker < Sinatra::Base
   private
 
   def self.app_settings
-    YAML.load(File.open('config/mongodb_broker.yml'))
+    recursive_symbolize_keys(YAML.load(File.open('config/mongodb_broker.yml')))
+  end
+
+  def self.recursive_symbolize_keys(h)
+    # This method was posted on stackoverflow by user 'pje'
+    # http://tinyurl.com/o4yyk4m
+    case h
+    when Hash
+      Hash[
+        h.map do |k, v|
+          [k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v)]
+        end
+      ]
+    when Enumerable
+      h.map { |v| recursive_symbolize_keys(v) }
+    else
+      h
+    end
   end
 
   def binding_password
